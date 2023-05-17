113dde685e3310dba48a0448596773aa1c27893c
Oluwatobi <tellajames63@gmail.com>
2023-05-17 15:06:07 +0100

    Made Changes to the header

--- components/header/header1.tsx
+++ components/header/header1.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Header} from '@mantine/core';
 import { Props } from '../interfaces/interface'
 import  useStyles  from './header.style'
+import {NavyBar} from '../navigation/navbar'
 
 export const FirstHeader: React.FC<Props> = ({ logoSrc, backgroundImage }) => {
   const { classes } = useStyles();
   return (
-<Header height='10vh' className={classes.styles21}
->
-  
+<Header height='25vh' className={classes.styles21} pos="relative">
+<NavyBar/>
 </Header>
   );
 };  
