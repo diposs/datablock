77e9fdfd6c97b4ea4f2f0ce6f09a2098733733fb
Oluwatobi <tellajames63@gmail.com>
2023-05-22 22:24:07 +0100

    Adding Polybase Authentification to the page

--- components/buttons/GSButton.tsx
+++ components/buttons/GSButton.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@mantine/core";
 import useStyles from '../style/GreaterThanSmall.style'
 
-export function GsButton() {
+export const GsButton = ({ ...rest }) =>  {
     const { classes } = useStyles();
     return (
-        <Button radius="xl" h={30}  className={classes.links}>GetStarted</Button>
+        <Button radius="xl" h={30}  className={classes.links} { ...rest }>GetStarted</Button>
     );
 }
