0bc341918804ad411287f181544515298e69e42d
diposs <onakunle.dipo@gmail.com>
2023-05-28 20:29:05 +0100

    Update container1.tsx

--- components/container/container1.tsx
+++ components/container/container1.tsx
@@ -1,10 +1,12 @@
 import { Center } from '@mantine/core';
+import { useFocusTrap } from '@mantine/hooks';
 import {SearchBar} from  '../inputs/searchbar';
 
 
 export const SearchContainer = ({ ...rest }) =>  {
+    const focusTrapRef = useFocusTrap();
     return (
-        <Center { ...rest }>
+        <Center ref={focusTrapRef} { ...rest }>
             <SearchBar/>
         </Center>
     );
