0146fdd54dadd0286ba536845b30a79586f9eeef
diposs <onakunle.dipo@gmail.com>
2023-05-28 20:23:56 +0100

    Update searchbar.tsx

--- components/inputs/searchbar.tsx
+++ components/inputs/searchbar.tsx
@@ -14,6 +14,7 @@ export function SearchBar() {
       icon={< IconSearch size="2.125rem" />}
       rightSection={< Searchiconright /> }
       miw="60vw"
+      data-autofocus
       placeholder="Search"
       value={query}
       onChange={handleQueryChange}
