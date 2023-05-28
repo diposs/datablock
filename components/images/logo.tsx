0ac1746b393a88d9558f78275945b567f09181eb
diposs <onakunle.dipo@gmail.com>
2023-05-28 17:21:48 +0100

    Update logo.tsx

--- components/images/logo.tsx
+++ components/images/logo.tsx
@@ -3,7 +3,7 @@ import { Image, AspectRatio } from '@mantine/core';
 export function HeaderLogo() {
   return (
     <AspectRatio ratio={78 / 45}>
-    <Image src="/HeadLogo.svg" alt="HeaderLogo" height={45} width={78}/>
+    <Image src="/HeadLogo.svg" alt="HeaderLogo" height={45} width={78} withPlaceholder/>
     </AspectRatio>
   );
 }
