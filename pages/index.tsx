66a6bdb4cea2ef8a5a4d89008515de2c0edc71c0
diposs <onakunle.dipo@gmail.com>
2023-05-29 10:03:43 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import {SearchContainer} from '../components/container/container1'
 export default function Home() {
   return (
   <>
-      <SearchContainer h='60vh' />
+      <SearchContainer />
       </>
   );
 }
