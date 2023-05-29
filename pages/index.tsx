f78857e87d38cf6704d44b534bb40a0be78a5674
diposs <onakunle.dipo@gmail.com>
2023-05-29 10:03:07 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import type { GetServerSideProps } from 'next'
+import { initializeStore } from '../stores/datastate'
 import {SearchContainer} from '../components/container/container1'
 export default function Home() {
   return (
@@ -5,4 +7,16 @@ export default function Home() {
       <SearchContainer h='60vh' />
       </>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const zustandStore = initializeStore()
+
+  return {
+    props: {
+      // the "stringify and then parse again" piece is required as next.js
+      // isn't able to serialize it to JSON properly
+      initialZustandState: JSON.parse(JSON.stringify(zustandStore.getState())),
+    },
+  }
 }
