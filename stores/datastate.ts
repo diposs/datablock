c02b856850db1fe8d04abaf4ba6e20354a43c586
diposs <onakunle.dipo@gmail.com>
2023-05-29 09:54:33 +0100

    Update datastate.ts

--- stores/datastate.ts
+++ stores/datastate.ts
@@ -1,8 +1,40 @@
-import create from "zustand";
-import { DataBlockType } from "../stateType/stores";
+import { createContext, useContext } from 'react'
+import { createStore, useStore as useZustandStore } from 'zustand'
 
-export const dataStore = create<DataBlockType>((set) => ({
-    mobilemenucontrol: false,
-    setMobilemenucontrol: (mobilemenucontrol) => set((state) => ({ ...state, mobilemenucontrol })),
-    removeMobilemenucontrol: () => set((state) => ({ ...state, mobilemenucontrol: false })),
-}));
+interface StoreInterface {
+  mobilemenucontrol: boolean
+  update: (mobilemenucontrol: boolean) => void
+
+}
+
+const getDefaultInitialState = () => ({
+  mobilemenucontrol: false,
+})
+
+export type StoreType = ReturnType<typeof initializeStore>
+
+const zustandContext = createContext<StoreType | null>(null)
+
+export const Provider = zustandContext.Provider
+
+export const useStore = <T>(selector: (state: StoreInterface) => T) => {
+  const store = useContext(zustandContext)
+
+  if (!store) throw new Error('Store is missing the provider')
+
+  return useZustandStore(store, selector)
+}
+
+export const initializeStore = (
+  preloadedState: Partial<StoreInterface> = {}
+) => {
+  return createStore<StoreInterface>((set, get) => ({
+    ...getDefaultInitialState(),
+    ...preloadedState,
+    update: (mobilemenucontrol) => {
+      set({
+        mobilemenucontrol: !!mobilemenucontrol,
+      })
+    },
+  }))
+}
