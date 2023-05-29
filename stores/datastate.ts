3e682b7f90cd9ddb028fb8792b9599c3df0778fe
diposs <onakunle.dipo@gmail.com>
2023-05-29 08:27:07 +0100

    Update datastate.ts

--- stores/datastate.ts
+++ stores/datastate.ts
@@ -1,5 +1,5 @@
 import create from "zustand";
-import { NearStoreType } from "../types/stores";
+import { DataBlockType } from "../types/stores";
 
 const dataStore = create<DataBlockType>((set) => ({
     mobilemenucontrol: false,
