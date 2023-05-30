a21cab81d23daa1341aa8bc961fddf8690a5149e
diposs <onakunle.dipo@gmail.com>
2023-05-30 23:34:51 +0100

    Update compound.ts

--- stores/compound.ts
+++ stores/compound.ts
@@ -4,7 +4,7 @@ export interface StoreInterface3 {
   inUser: boolean
   updateinUser: (inUser: boolean) => void
   pKey: string | null | undefined
-  updatepKey: (inUser: string) => void
+  updatepKey: (pKey: string | null | undefined) => void
 }
 
 export const createcompound: StateCreator<StoreInterface3> = (set, get) => ({
