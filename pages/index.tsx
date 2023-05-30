e5a25d4e024f6c3f09aa63897b7013e112ca3cb3
diposs <onakunle.dipo@gmail.com>
2023-05-30 08:34:40 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetStaticProps } from 'next'
+import { useBoundStore } from '../stores/datastate'
 import {SearchContainer} from '../components/container/container1'
 export default function Home() {
   return (
-  <>
       <SearchContainer />
-      </>
   );
 }
 
+export const getStaticProps: GetStaticProps = () => {
+  const zustandStore = useBoundStore()
+  return {
+    props: {
+      // the "stringify and then parse again" piece is required as next.js
+      // isn't able to serialize it to JSON properly
+      initialZustandState: JSON.parse(JSON.stringify(zustandStore.getState())),
+    },
+  }
+}
