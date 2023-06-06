5ed8c91e0145c659da8ede600b7a16320f89b66b
diposs <onakunle.dipo@gmail.com>
2023-06-06 13:28:41 +0100

    Update croned.ts

--- pages/api/croned.ts
+++ pages/api/croned.ts
@@ -1,97 +1,45 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
 import { ethPersonalSign } from '@polybase/eth'
-
 import { Polybase } from "@polybase/client";
-
-import { ethers, Wallet } from "ethers";
+import { ethers } from "ethers";
 
 type Data = {
-
-  name: string[]
-
+  name: string
 }
 
 export default async function handler(
-
   req: NextApiRequest,
-
   res: NextApiResponse<Data>
-
 )
-
 {
-
   const trydipo:any = await process.env.NEXT_PUBLIC_ACCOUNT_PRIVATE_KEY;
-
-  var url = 'https://rpc.ankr.com/eth';
-
+  var url = 'https://ethereum.publicnode.com/';
   var provider = await new ethers.providers.JsonRpcProvider(url);
   const dpkey = '0x89de820323237a0e6cab8c5f29dfbf2f026f8c1da20c01f5b06b31877252a9d0f493bf95b625b667b1bdb3fb1593553bda1f056220cb2aa0e680316dba8b9a2c'
-
   const db = await new Polybase({
-
   defaultNamespace: "pk/0x89de820323237a0e6cab8c5f29dfbf2f026f8c1da20c01f5b06b31877252a9d0f493bf95b625b667b1bdb3fb1593553bda1f056220cb2aa0e680316dba8b9a2c/DataBlockV2.1",
-
 });
-
   db.signer((data) => {
-
   return {
-
     h: 'eth-personal-sign',
-
-    sig: ethPersonalSign(trydipo, data)
-
+    sig: ethPersonalSign(wallet.privateKey, data)
   }
-
 });
-
  
-
   let wedd =  0;
-const data6 = await db.collection('Chaintrack').record('ETH').get()
-
+  while(wedd <= 20000){
+    try{
+      const data6 = await db.collection('Chaintrack').record('ETH').get()
   let we2 = data6.data.blockNumber[0];
-
       let we = we2 + 1;
-  let k = []
-  while(wedd <= 11){
-
-    try{
-
-      
-
   const dsd = await provider.getBlock(we);
-      
-
   const doinge = {'parentHash':dsd.parentHash, 'hash': dsd.hash, 'numbers':dsd.number, 'timestamp':dsd.timestamp};
-
   const daada = JSON.stringify(doinge);
-k.push(daada);
-
+  const data7 = await db.collection('IndexBlock').create([db.collection('LevelnMembership').record('Membership'),db.collection('Chaintrack').record('ETH'),db.collection('CompilerUser').record(dpkey),db.collection('AvailableJobs').record('Jobs'),daada]);
     }catch (e: any) {
-
       console.log(e);
-
     }
-     we += 1;
-
   wedd += 1;
-
     };
-  let kc = 0;
-  while(kc <= 11){
-    try{
-      
-      const data7 = await db.collection('IndexBlock').create([db.collection('LevelnMembership').record('Membership'),db.collection('Chaintrack').record('ETH'),db.collection('CompilerUser').record(dpkey),db.collection('AvailableJobs').record('Jobs'),k[kc]]);
-      
-    }catch(e:any){
-      console.log(e);
-    }
-    kc += 1;
-  }
-
-  res.status(200).json({ name: k });
-
+  res.status(200).json({ name: 'John Doe' });
 }
