857b46d3299e0e1a892c73a3d74c4cbbf78ae2e8
diposs <onakunle.dipo@gmail.com>
2023-06-09 15:08:21 +0100

    Update cron.ts

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -17,7 +17,7 @@ export default async function handler(
   var provider = await new ethers.providers.JsonRpcProvider(url);
   const dpkey = '0x89de820323237a0e6cab8c5f29dfbf2f026f8c1da20c01f5b06b31877252a9d0f493bf95b625b667b1bdb3fb1593553bda1f056220cb2aa0e680316dba8b9a2c'
   const db = await new Polybase({
-  defaultNamespace: "pk/0x89de820323237a0e6cab8c5f29dfbf2f026f8c1da20c01f5b06b31877252a9d0f493bf95b625b667b1bdb3fb1593553bda1f056220cb2aa0e680316dba8b9a2c/DataBlockV2.1",
+  defaultNamespace: "pk/0x89de820323237a0e6cab8c5f29dfbf2f026f8c1da20c01f5b06b31877252a9d0f493bf95b625b667b1bdb3fb1593553bda1f056220cb2aa0e680316dba8b9a2c/DataBlockV3",
 });
   db.signer((data) => {
   return {
@@ -32,19 +32,14 @@ export default async function handler(
 
     try{
 
-      const data6 = await db.collection('Chaintrack').record('ETH').get()
 
-  let we2 = data6.data.blockNumber[0];
-
-      let we = we2 + 1;
-
-  const dsd = await provider.getBlock(we);
+  const dsd = await provider.getBlock(wedd);
 
   const doinge = {'parentHash':dsd.parentHash, 'hash': dsd.hash, 'numbers':dsd.number, 'timestamp':dsd.timestamp};
 
   const daada = JSON.stringify(doinge);
 
-  const data7 = await db.collection('IndexBlock').create([db.collection('LevelnMembership').record('Membership'),db.collection('Chaintrack').record('ETH'),db.collection('CompilerUser').record(dpkey),db.collection('AvailableJobs').record('Jobs'),daada]);
+  const data7 = await db.collection('IndexBlock').create([db.collection('Chaintrack').record('0ETH'),db.collection('CompilerUser').record(dpkey),daada]);
 
     }catch (e: any) {
 
